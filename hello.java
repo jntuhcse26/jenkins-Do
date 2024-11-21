2dc82a7fe444cdc1475cd220aaa037bfb9ada7c7
jntuhcse26 <cejcse26@gmail.com>
2024-11-21 16:23:43 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 class hello
-{public static void main(string args[])
 {
-    system.out.println("hello world");
+    public static void main(String args[])
+{
+    System.out.println("hello world");
 }
 }
